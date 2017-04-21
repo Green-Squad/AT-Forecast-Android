e0747ca6c9c6cd7b687252613ef6a7623a746a88
Kyle Dornblaser <kdornblaser@gmail.com>
2017-04-20 21:37:51 -0400

    Initial location permision code.

--- app/src/main/java/com/greensquad/atforecast/MainActivity.java
+++ app/src/main/java/com/greensquad/atforecast/MainActivity.java
@@ -1,6 +1,9 @@
 package com.greensquad.atforecast;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,12 +15,17 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
 import com.greensquad.atforecast.base.BaseActivity;
 import com.greensquad.atforecast.fragments.StateListFragment;
 
 import butterknife.OnItemClick;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity  implements ConnectionCallbacks, OnConnectionFailedListener {
 
     static final String LOG_TAG = MainActivity.class.getSimpleName();
 
@@ -25,11 +33,21 @@ public class MainActivity extends BaseActivity {
     private NavigationView drawerList;
     private DrawerLayout drawerLayout;
     private Toolbar toolbar;
+    private GoogleApiClient mGoogleApiClient = null;
+    protected Location mLastLocation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
 
         drawerList = (NavigationView) findViewById(R.id.nav_view);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -52,7 +70,16 @@ public class MainActivity extends BaseActivity {
         switch (item.getItemId()) {
             // action with ID action_refresh was selected
             case R.id.gps:
-                Toast.makeText(this, "Find by location", Toast.LENGTH_SHORT).show();
+
+                try {
+                    mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                            mGoogleApiClient);
+                    if (mLastLocation != null) {
+                        Toast.makeText(this, String.valueOf(mLastLocation.getLatitude()) + " " + String.valueOf(mLastLocation.getLongitude()), Toast.LENGTH_SHORT).show();
+                    }
+                } catch(SecurityException se) {
+
+                }
                 break;
             default:
                 break;
@@ -118,5 +145,30 @@ public class MainActivity extends BaseActivity {
     @Override
     protected ActionBarDrawerToggle getDrawerToggle() {
         return mDrawerToggle;
+    }
+
+    protected void onStart() {
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
